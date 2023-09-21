92c0d8080c57d90a54936968317b7ad2a6950581
Piotr Krupa <piotrkrupa.email@gmail.com>
2023-09-21 15:25:15 +0200

    Module 11 - first commit

---
+++ src/components/Container/Container.js
@@ -0,0 +1,9 @@
+import styles from './Container.module.scss'
+
+const Container = props => {
+    return (
+        <div className={styles.Container}>{props.children}</div>
+    )
+}
+
+export default Container; 
